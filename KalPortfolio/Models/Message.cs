99be4dbf191c7e55fd8a5e81597ef6cbe6117a4b
Kal Vassar <kvassar@hbs.net>
2023-01-19 12:54:14 -0600

    Added some additional things to message section including an image carsouel.

--- KalPortfolio/Models/Message.cs
+++ KalPortfolio/Models/Message.cs
@@ -4,12 +4,18 @@ namespace KalPortfolio.Models
 {
     public class Message
     {
-        [Required(ErrorMessage = "Please give your name."), RegularExpression(@"^[A-Z]+[a-zA-Z'\s/]*$")]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Please give your first name."), RegularExpression(@"^[A-Z]+[a-zA-Z'\s/]*$")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please give your last name."), RegularExpression(@"^[A-Z]+[a-zA-Z'\s/]*$")]
+        public string LastName { get; set; }
+
         [Required(ErrorMessage = "Please give your message a subject."), RegularExpression(@"^[A-Z]+[a-zA-Z'\s/]*$")]
         public string Subject { get; set; }
+
         [Required(ErrorMessage = "Please give your Email.")]
         public string Email { get; set; }
+        
         [Required(ErrorMessage = "What would you like to say?"), RegularExpression(@"^[A-Z]+[a-zA-Z'\s/]*$")]
         public string ViewerMessage { get; set; }
 
