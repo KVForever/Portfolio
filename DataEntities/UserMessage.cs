984f6f644eb9f27ad9ff6c8b789695b001b528e4
Kal Vassar <kvassar@hbs.net>
2023-02-15 16:57:44 -0600

    I added some validations

--- DataEntities/UserMessage.cs
+++ DataEntities/UserMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataEntities;
 
@@ -8,13 +9,18 @@ public partial class UserMessage
     
     public Guid Id { get; set; }
 
+    [Required, RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
     public string FirstName { get; set; } = null!;
 
+    [Required, RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
     public string LastName { get; set; } = null!;
 
+    [EmailAddress]
     public string? Email { get; set; }
 
+    [Required, RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$")]
     public string Subject { get; set; } = null!;
 
+    [Required, RegularExpression(@"^[a-zA-Z''-'\S]{1,600}$")]
     public string Message { get; set; } = null!;
 }
