a63f879a1d0bdfc4ce3adfe73c442b0dfe74cb75
Kal Vassar <kvassar@hbs.net>
2023-04-04 11:55:38 -0500

    finishing up the home screen buttons for the user view.

--- KalPortfolio/wwwroot/js/site.js
+++ KalPortfolio/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(function () {
+    var navBar = $(".home-nav")
+    var findMeLinks = $(".find-me-links");
+    var verticalLine = $(".vertical-line")
+    var findMe = $(".find-me")
+    var navBarTop = navBar.offset().top;
+    var windowHeight = $(window).height();
+    var count = 0;
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= (5) && count < 1) {
+            findMeLinks.addClass("links-animate");
+            verticalLine.addClass("line-animate");
+            findMe.addClass("find-me-animate")
+            count++;
+        }
+    });
+});
